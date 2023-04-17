a9340e1f6b2e6b0555348228718b93c1be6581b7
hv90 <61795418+hv0l@users.noreply.github.com>
2023-04-17 14:23:22 +0200

    Create telegram_monitor.py

---
+++ telegram_monitor.py
@@ -0,0 +1,42 @@
+import asyncio
+import json
+import logging
+from telethon import TelegramClient
+from telethon.tl.functions.messages import GetDialogsRequest
+
+# Setup logging
+logging.basicConfig(filename='telegram_monitor.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# Load configuration
+with open('config.json', 'r') as config_file:
+    config = json.load(config_file)
+
+api_id = config['api_id']
+api_hash = config['api_hash']
+phone_number = config['phone_number']
+second_account_id = config['second_account_id']
+
+def check_keywords(text: str) -> bool:
+    keywords = ["word1", "word2", "word3"]
+    return any(keyword.lower() in text.lower() for keyword in keywords)
+
+async def search_messages(client, entity):
+    async for message in client.iter_messages(entity, limit=None):
+        if check_keywords(message.text):
+            await client.send_message(second_account_id, f"Warn: {message.text}")
+            logging.info(f"Warn sent: {message.text}")
+
+async def main():
+    try:
+        async with TelegramClient("anon", api_id, api_hash) as client:
+            dialogs = await client(GetDialogsRequest(offset_date=None, limit=100))
+            for dialog in dialogs.dialogs:
+                entity = await client.get_entity(dialog.peer)
+                await search_messages(client, entity)
+        logging.info("Execution completed")
+    except Exception as e:
+        logging.error(f"Error: {e}")
+
+if __name__ == "__main__":
+    logging.info("Starting telegram_monitor")
+    asyncio.run(main())
