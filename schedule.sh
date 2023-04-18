8dd5eba6a0a2afe5bad3f9b8be9ca760bffc6b78
hv90 <61795418+hv0l@users.noreply.github.com>
2023-04-18 11:50:47 +0200

    Update schedule.sh

--- schedule.sh
+++ schedule.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
 
+# Check if the keywords config file exists, if not, create it
+if [ ! -f "keywords.json" ]; then
+    echo "Keywords config file not found. Creating a new one."
+
+    # Prompt the user for the number of keywords
+    echo "Enter the number of keywords you want to search for:"
+    read num_keywords
+
+    # Prompt the user for the keywords and save them in an array
+    keywords=()
+    for ((i=1; i<=$num_keywords; i++)); do
+        echo "Enter keyword $i:"
+        read keyword
+        keywords+=("\"$keyword\"")
+    done
+
+    # Create the keywords.json file with the specified keywords
+    echo "{" > keywords.json
+    echo "  \"keywords\": [" >> keywords.json
+    echo "    $(IFS=,; echo "${keywords[*]}")" >> keywords.json
+    echo "  ]" >> keywords.json
+    echo "}" >> keywords.json
+fi
+
 # Create a temporary crontab file
 crontab -l > mycron.tmp
 
 # Check if the script is already scheduled, if not, add the schedule
-if ! grep -q "run_telegram_monitor.sh" mycron.tmp; then
-    echo "0 * * * * /path/to/your/run_telegram_monitor.sh" >> mycron.tmp
+if ! grep -q "schedule.sh" mycron.tmp; then
+    echo "0 * * * * /path/to/your/schedule.sh" >> mycron.tmp
     crontab mycron.tmp
     echo "Cron job added"
 else
